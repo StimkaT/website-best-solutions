ed915625fafb9b83d1c0ff8feb978aa728c03b4e
Николай <kolyatola@mail.ru>
2024-05-31 10:16:52 +0300

    start to creation website

---
+++ src/app/states/constants.ts
@@ -0,0 +1,13 @@
+type CurrencyType = '₹' | '$' | '€'
+
+export const currency: CurrencyType = '$'
+
+export const currentYear = new Date().getFullYear()
+
+export const developedByLink = 'https://coderthemes.com/'
+
+export const createdBy = 'Coderthemes'
+
+export const buyLink = ""
+
+export const basePath: string = ""
